6965afb6cacb66d1e5e1705dd61072cebc8d00ca
Kevin Le <149617068+kele6026@users.noreply.github.com>
2025-09-04 13:17:07 -0600

    Update edc_create_pat_records.sql

--- edc_create_pat_records.sql
+++ edc_create_pat_records.sql
@@ -3,21 +3,21 @@ with
     -- GET DATA FROM CURRENT ATE
     gather_current_ate_data as (
         select
-            learner_account_id,
+            account_id,
             current_ct_status_c,
             current_ct_phase_c,
             years_since_high_school_graduation_ate_c,
 
             -- POPULATE THIS EVERY YEAR
-            'Spring' as term_to_create,
-            '2024-25' as year_to_create,
-            date(2025, 04, 01) as start_date_of_term_to_create,
+            'Summer' as term_to_create,
+            '2025-26' as year_to_create,
+            date(2025, 07, 01) as start_date_of_term_to_create,
 
             -- END MANUAL UPDATE
             ate_id as previous_academic_term_enrollment_c,
-            ate_at_id as current_ate_at,
+            at_id as current_ate_at,
             'Undergraduate' as student_academic_level,
-            learner_contact_id,
+            contact_id,
             ate_school_id,
             ct_coach_ate_c,
             ct_case_c,
@@ -29,10 +29,16 @@ with
             major_c,  -- text
             second_major_category_c,  -- picklist
             second_major_c,  -- text
-            minor_c  -- text
+            minor_c,  -- text
+
+            -- ACADEMIC TERM INFORMATION
+            at_name as current_at_name,
+            academic_calendar_ate_c as current_academic_calendar
+
         from `prod_core.fct_edcloud_academic_term_enrollment`
         where
             current_at_c = true
+            and grade_ate_c != '12th Grade' -- filter out graduating seniors when generating summer PAT
             and (
                 (
                     current_ct_status_c = 'Active'
@@ -40,8 +46,12 @@ with
                 )
                 or (
                     current_ct_status_c = 'Inactive'
-                    and current_ct_phase_c in ('Post Secondary', 'Alumni')
-                    and years_since_high_school_graduation_ate_c < 5.99
+                    and current_ct_phase_c = 'Post Secondary'
+                    and years_since_high_school_graduation_ate_c <= 5.99
+                )
+                or (
+                    current_ct_phase_c = 'Alumni'
+                    and years_since_high_school_graduation_ate_c <= 5.99
                 )
             )
     ),
@@ -49,43 +59,24 @@ with
     most_recent_credit as (
         select
             account_id,
-            cumulative_credits_awarded_most_recent_pc
+            most_recent_num_cumulative_credits_pc
         from `data-studio-260217.prod_core.dim_scholar`
     ),
 
     join_credit as ( 
         select
             gather_current_ate_data.*,
-            most_recent_credit.cumulative_credits_awarded_most_recent_pc
+            most_recent_credit.most_recent_num_cumulative_credits_pc
         from gather_current_ate_data
         left join most_recent_credit 
-        on most_recent_credit.account_id = gather_current_ate_data.learner_account_id
+        on most_recent_credit.account_id = gather_current_ate_data.account_id
     ),
 
     -- GET INFORMATION FROM GLOBAL ACADEMIC TERM
-    current_at_data as (
-        select
-            at_id,
-            at_name as current_at_name,
-            academic_calendar_c as current_academic_calendar
-        from `data-studio-260217.prod_staging.stg_edcloud__academic_term`
-    ),
-
-    join_current_at as (
-        select
-            join_credit.*,
-            current_at_data.current_at_name,
-            current_at_data.current_academic_calendar
-        from join_credit
-        left join
-            current_at_data
-            on current_at_data.at_id = join_credit.current_ate_at
-    ),
-
     term_ids as (
         select
             at_id,
-            academic_year_id,
+            ay_id,
             at_name,
             season,
             academic_calendar_c,
@@ -97,31 +88,52 @@ with
     -- JOIN TERM DATA FOR NEXT TERM
     join_term_ids as (
         select
-            join_current_at.*,
+            join_credit.*,
             term_ids.at_id,
-            term_ids.academic_year_id,
+            term_ids.ay_id,
             term_ids.at_name as at_name_to_create,
             term_ids.season as at_term_to_create,
             term_ids.start_date as enrollment_date,
             term_ids.exit_date
-        from join_current_at
+        from join_credit
         inner join
             term_ids
-            on join_current_at.start_date_of_term_to_create = term_ids.start_date
-            and join_current_at.current_academic_calendar = term_ids.academic_calendar_c
+            on join_credit.start_date_of_term_to_create = term_ids.start_date
+            and join_credit.current_academic_calendar = term_ids.academic_calendar_c
+    ),
+
+    -- JOIN ACADEMIC YEAR ENROLLMENT
+    get_aye as (
+        select
+            aye_id as academic_year_enrollment_c,
+            ay_id,
+            account_id
+        from `prod_core.fct_edcloud_academic_year_enrollment`
+    ),
+
+    join_aye as (
+        select
+            join_term_ids.*,
+            get_aye.academic_year_enrollment_c
+        from join_term_ids
+        left join get_aye
+        on
+            get_aye.account_id = join_term_ids.account_id
+            and get_aye.ay_id = join_term_ids.ay_id
     ),
 
     prep_data as (
         select
             at_name_to_create,
-            learner_account_id,
+            account_id,
             ct_status_ate_c,
             previous_academic_term_enrollment_c,
+            academic_year_enrollment_c,
             at_id,
-            academic_year_id,
+            ay_id,
             student_academic_level,
             ct_case_c,
-            learner_contact_id,
+            contact_id,
             owner_id,
             enrollment_date,
             exit_date,
@@ -153,7 +165,7 @@ with
             case
                 when current_ct_status_c != 'Active'
                 then null
-                else cumulative_credits_awarded_most_recent_pc
+                else most_recent_num_cumulative_credits_pc
             end as cumulative_credits_awarded_all_terms_c,
 
             -- following fields null for alumni
@@ -163,7 +175,7 @@ with
             case
                 when current_ct_phase_c = 'Alumni' then null else ct_coach_ate_c
             end as ct_coach_ate_c
-        from join_term_ids
+        from join_aye
     )
 
 select *
@@ -172,7 +184,7 @@ from  prep_data
     -- check_record_count as (
     --     select
     --         at_name_to_create,
-    --         count(learner_account_id) as n_students,
+    --         count(account_id) as n_students,
     --         count(*) as n_records
     -- from prep_data
     -- group by 1
